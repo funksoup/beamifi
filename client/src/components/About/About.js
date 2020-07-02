e0115aae9cc82b37fb328d057540d01128b00f5d
rf-spuds <rfspuds@gmail.com>
2020-07-01 22:31:11 -0400

    Skeleton of about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -5,7 +5,64 @@ function About() {
   return (
     <div>
     <Navbar />
-      About Page
+    
+
+
+      <div className="container">
+      <h1>Meet the Team!</h1>
+      <div className="row">
+        <div className="card-horizontal z-depth-4" id="team-member1">
+          <div className="card-image">
+            <img src="https://lorempixel.com/200/200/nature/6" alt="" className="responsive-img" id="team-member-1-img"/>
+          </div>
+            <div className="card-stacked">
+              <div className="card-content"><p>I am teh best developer of all time!!!!!!</p></div>
+              <div className="card-action"><a href="" ahref="#">Link to my github and linked in and etc</a></div>
+            </div>
+        </div>
+        <div className="card-horizontal z-depth-4" id="team-member2">
+          <div className="card-image">
+            <img src="https://lorempixel.com/200/200/nature/6" alt="" className="responsive-img" id="team-member-2-img"/>
+          </div>
+            <div className="card-stacked">
+              <div className="card-content"><p>I am teh best developer of all time!!!!!!</p></div>
+              <div className="card-action"><a href="" ahref="#">Link to my github and linked in and etc</a></div>
+            </div>
+        </div>
+        <div className="card-horizontal z-depth-4" id="team-member3">
+          <div className="card-image">
+            <img src="https://lorempixel.com/200/200/nature/6" alt="" className="responsive-img" id="team-member-3-img"/>
+          </div>
+            <div className="card-stacked">
+              <div className="card-content"><p>I am teh best developer of all time!!!!!!</p></div>
+              <div className="card-action"><a href="" ahref="#">Link to my github and linked in and etc</a></div>
+            </div>
+        </div>
+        <div className="card-horizontal z-depth-4" id="team-member4">
+          <div className="card-image">
+            <img src="https://lorempixel.com/200/200/nature/6" alt="" className="responsive-img" id="team-member-4-img"/>
+          </div>
+            <div className="card-stacked">
+              <div className="card-content"><p>I am teh best developer of all time!!!!!!</p></div>
+              <div className="card-action"><a href="" ahref="#">Link to my github and linked in and etc</a></div>
+            </div>
+        </div>
+        <div className="card-horizontal z-depth-4" id="team-member5">
+          <div className="card-image">
+            <img src="https://lorempixel.com/200/200/nature/6" alt="" className="responsive-img" id="team-member-5-img"/>
+          </div>
+            <div className="card-stacked">
+              <div className="card-content"><p>I am teh best developer of all time!!!!!!</p></div>
+              <div className="card-action"><a href="" ahref="#">Link to my github and linked in and etc</a></div>
+            </div>
+        </div>
+      </div>
+
+
+
+
+      </div>
+
     </div>
   );
 }
