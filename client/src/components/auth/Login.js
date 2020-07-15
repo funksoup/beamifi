9615e7079280142cbdbddb1bbd31086b7f8ac9bd
wuyaheng <wuyaheng2016@gmail.com>
2020-07-14 20:01:36 -0400

    fixed login issue

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,49 +1,57 @@
-import React, { useState, useContext, useEffect } from 'react'
-import AuthContext from '../../context/auth/authContext'
-import AlertContext from '../../context/alert/alertContext'
+import React, { useState, useContext, useEffect }from 'react';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
 import bg from "../../asset/bg.png"
 
-const Login = (props) => {
 
+
+const Login = (props) => {
     const alertContext = useContext(AlertContext);
     const authContext = useContext(AuthContext);
 
     const { setAlert } = alertContext;
-
     const { login, error, clearErrors, isAuthenticated } = authContext;
 
     useEffect(() => {
         if(isAuthenticated) {
             props.history.push('/');
         }
-        if(error==='Invalid Credentials') {
-            setAlert(error, 'danger');
-            clearErrors();
-        }
-        // eslint-disable-next-line
-    },[error, isAuthenticated, props.history]); 
+
+        if(error === 'Invalid Credentials') {
+         setAlert(error, 'danger');
+         clearErrors();
+    }
+
+    // eslint-disable-next-line
+}, [error, isAuthenticated, props.history]);
 
 
     const [user, setUser] = useState({
-        email:'',
-        password:''
+        email: '',
+        password: '',
+        
     });
 
     const { email, password } = user;
 
-    const onChange = e => setUser({...user, [e.target.name]: e.target.value }); 
+    const onChange = e => setUser ({...user, [e.target.name]: e.target.value });
 
     const onSubmit = e => {
+
+//console.log("test")
         e.preventDefault();
-        if(email==='' || password==="") {
-            setAlert('Please fill in all fields','danger')
+        if(email === '' || password === '') {
+            setAlert('Please fill in all fields', 'danger');
         } else {
             login({
                 email,
-                password 
-            })  
-        }
-    }
+                password
+            });
+        }       
+    };
+
+
+
 
     return (
 
@@ -66,41 +74,28 @@ const Login = (props) => {
 
      
         <div className='col-md-4 mt-3' id="heading">
-          <div className="form-container mt-3">
-            <h1 className="text-center">
-                Account <span style={{color: "#ff5e62"}}>Login</span>
-            </h1>
-            <form className="mt-2" onSubmit={onSubmit}> 
 
-                <div className="form-group">
-                    <label htmlFor="email">Email Address</label>
-                    <input type="email" name="email" value={email} onChange={onChange} required/>
-                </div>
 
-                <div className="form-group">
-                    <label htmlFor="password">Password</label> 
-                    <input type="password" name="password" value={password} onChange={onChange} required/>
-                </div>
-
-                <input type="submit" value="Login" className="btn btn-primary btn-block"/>
-
-            </form>
+        <div className='form-container'>
+        <h1>
+            Account <span className="text-primary">Login</span>
+        </h1>
+        <form onSubmit={onSubmit}>
+            <div className="form-group">
+                <label htmlFor="email">Email Address</label>
+                <input type="email" name="email" value={email} onChange={onChange} required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" value={password} onChange={onChange} required />
+            </div>
+            <input type="submit" value="Login" className="btn btn-primary btn-block" />
+        </form>
+            
         </div>
-
-
-
-
-          </div>
-       
-
-          </div>
-
-
-
-
-
+        </div>
+    </div>
     )
 }
-
 
 export default Login;
