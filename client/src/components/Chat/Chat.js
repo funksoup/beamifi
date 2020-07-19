86ab8c4d80bea7c9a6b7b33fe30c3824e753daa1
wuyaheng <wuyaheng2016@gmail.com>
2020-07-18 21:28:37 -0400

    fixed scroll to bottom issue

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -57,7 +57,7 @@ const Chat = ({ location }) => {
   return (
 
     <div className="outerContainer">
-      <div className="container">
+      <div className="container chatroom">
           <InfoBar room={room} />
           <Messages messages={messages} name={name} />
           <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
